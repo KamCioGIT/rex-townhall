f08cf1a55ecd96f53e6875141c07a0183b22f9f6
RexshackGaming <rexshackuk@gmail.com>
2024-06-15 21:20:13 +0100

    update

--- server/server.lua
+++ server/server.lua
@@ -6,8 +6,11 @@ local RSGCore = exports['rsg-core']:GetCoreObject()
 RegisterNetEvent('rex-townhall:server:applyjob', function(data)
     local src = source
     local Player = RSGCore.Functions.GetPlayer(src)
+    local firstname = Player.PlayerData.charinfo.firstname
+    local lastname = Player.PlayerData.charinfo.lastname
     if not Player then return end
     local ped = GetPlayerPed(src)
     Player.Functions.SetJob(data.job, data.grade)
     TriggerClientEvent('rex-townhall:client:notifyjob', src, data.jobtitle)
+    TriggerEvent('rsg-log:server:CreateLog', 'newjob', 'Player New Job', 'green', firstname..' '..lastname..' is now a '..data.jobtitle)
 end)
